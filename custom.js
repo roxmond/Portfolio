1496dec6ce26f3f4fa51f887c86cca7f18c75c98
roxmond <60523691+roxmond@users.noreply.github.com>
2020-11-07 17:00:43 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,7 @@
+document.createElement("h_teo");
+var mybox = document.createElement("div");
+window.onload = function() {
+    var div = document.createElement("mybox");
+    mybox.appendChild(div);
+
+}
