3ea02147f198862044e2d4783d16f70e96f93994
roxmond <60523691+roxmond@users.noreply.github.com>
2023-04-18 14:55:01 +0300

    Delete custom.js

--- custom.js
+++
@@ -1,7 +0,0 @@
-document.createElement("h_teo");
-var mybox = document.createElement("div");
-window.onload = function() {
-    var div = document.createElement("mybox");
-    mybox.appendChild(div);
-
-}
